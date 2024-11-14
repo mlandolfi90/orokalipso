d8250c78504be0037fd6f3558d78f470ee45ffa0
mlandolfi90 <mllandolfi90@gmail.com>
2024-11-14 12:07:04 +0000

    Avance 14-11-24

---
+++ orokalipso/orokalipso/doctype/ofinicinas_configuraciones/ofinicinas_configuraciones.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, mlandolfi90 and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Ofinicinas configuraciones", {
+// 	refresh(frm) {
+
+// 	},
+// });
