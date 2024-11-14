266772402909e32419c8b54047d7f91c9ee48ead
mlandolfi90 <mllandolfi90@gmail.com>
2024-11-14 21:32:25 +0000

    Exportacion de Algoritmos

--- orokalipso/hooks.py
+++ orokalipso/hooks.py
@@ -242,3 +242,13 @@ app_license = "mit"
 # 	"Logging DocType Name": 30  # days to retain logs
 # }
 
+fixtures = [{
+        "doctype": "Custom Field",
+    },
+    {
+        "doctype": "Server Script",
+    },
+    {
+        "doctype": "Client Script",
+    }
+]
