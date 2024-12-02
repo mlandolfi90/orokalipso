939abdb7b0856c53e60245cab11b5b22bb50bfee
mlandolfi90 <mllandolfi90@gmail.com>
2024-12-02 12:23:44 +0000

    Descripción de los cambios realizados

--- orokalipso/orokalipso/doctype/transferir_oro_orokalipso/transferir_oro_orokalipso.js
+++ orokalipso/orokalipso/doctype/transferir_oro_orokalipso/transferir_oro_orokalipso.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2024, mlandolfi90 and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Comprar Oro", {
+// frappe.ui.form.on("Transferir Oro Orokalipso", {
 // 	refresh(frm) {
 
 // 	},
