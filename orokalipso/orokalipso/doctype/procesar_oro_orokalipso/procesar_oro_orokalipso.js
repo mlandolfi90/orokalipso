99eaf99d537538fa3dc391a6fc6f9c2f92b9f190
mlandolfi90 <mllandolfi90@gmail.com>
2024-12-21 19:46:00 +0000

    Añadidos nuevos doctypes y formatos de impresión

---
+++ orokalipso/orokalipso/doctype/procesar_oro_orokalipso/procesar_oro_orokalipso.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, mlandolfi90 and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Procesar Oro Orokalipso", {
+// 	refresh(frm) {
+
+// 	},
+// });
--- orokalipso/orokalipso/doctype/procesar_oro_piezas_in/procesar_oro_piezas_in.py
+++ orokalipso/orokalipso/doctype/procesar_oro_piezas_in/procesar_oro_piezas_in.py
@@ -5,5 +5,5 @@
 from frappe.model.document import Document
 
 
-class TransferirOroItemsRelacion(Document):
+class ProcesarOropiezasin(Document):
 	pass
